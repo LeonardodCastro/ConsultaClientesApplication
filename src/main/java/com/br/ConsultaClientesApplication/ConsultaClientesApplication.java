f454ad2f9352f62ba8cd18479ae23959a9c2b0ea
leonardodecastrodasilveira <dev.leonardocastro@gmail.com>
2023-06-26 11:23:46 -0300

    Initial commit

---
+++ src/main/java/com/br/ConsultaClientesApplication/ConsultaClientesApplication.java
@@ -0,0 +1,14 @@
+package com.br.ConsultaClientesApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsultaClientesApplication {
+
+	public static void main(String[] args) {
+
+        SpringApplication.run(ConsultaClientesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/ConsultaClientesApplication/ConsultaClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.ConsultaClientesApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultaClientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
